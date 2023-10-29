fb33bac61a4af04b268d18ce575f84b18912c48e
Tartl <fejtaj.06@spst.eu>
2023-10-29 17:19:32 +0100

    1.Level
    Upravil jsem layout 1.levelu, místo toho panáka bude enemy

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,7 +13,7 @@ public class PlayerMovement : MonoBehaviour
 
     bool jump = false;
 
-    float actionCooldown = 1f;
+    float actionCooldown = 0.75f;
     float timeSinceAction = 0f;
 
     bool crouch = false;
@@ -37,13 +37,13 @@ public class PlayerMovement : MonoBehaviour
              
         }
         
-       /* if (Input.GetButtonDown("Crouch")) // crouching not needed
+        if (Input.GetButtonDown("Crouch")) // crouching not needed
         {
             crouch = true;
         } else if (Input.GetButtonUp("Crouch"))
         {
             crouch = false;
-        }*/
+        }
     }
 
     public void OnLanding ()
